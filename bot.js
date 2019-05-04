ea4eb8b19af3ebee02c253b000a790e7d2822dba
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-04 13:03:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setstatus("idle");
-
+client.user.setstatus("dnd");
 client.user.setGame("Lá Famillia.");
 });
 
