50503d8305da5c93a32ddc314ea32b71b24d65c8
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-08 21:09:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Lá Famillia.`,"http://twitch.tv//idk");
+client.user.setGame(`Lá Famillia`,"http://twitch.tv//idk");
 });
 
 
